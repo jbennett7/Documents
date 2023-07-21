722db70cdff98a349f25d5e1c223edcdbbd2748b
Joseph Bennett <jbennett@jbennettconsulting.com>
2023-07-21 18:21:16 -0400

    Scan single and multiple pages.

--- src/scanner.py
+++ src/scanner.py
@@ -8,14 +8,42 @@ class NoDeviceFoundError(Exception):
 class Scanner(object):
     def __init__(self):
         self.sane_version = sane.init()
-        self.__initialize_device()
+        # Use the id of the option as the key
+        self.__key = lambda x: x[0]
 
-    def __initialize_device(self):
+    def initialize_device(self) -> None:
+        """initialize_device() -> None"""
         device = sane.get_devices()
         if device:
-            self.__dev = sane.open(device)
+            self.__dev = sane.open(device[0][0])
         else:
             raise NoDeviceFoundError
+
+    def get_device_parameters(self) -> tuple[str, int, tuple[int, int], int, int]:
+        """get_device_parameters() -> tuple[str, int, tuple[int, int], int, int]"""
+        self.__params = self.__dev.get_parameters()
+        return self.__params
+
+    def get_options(self) -> list[tuple[int, str, str, str, str, bytes, str, str]]:
+        """SANE Device Options:
+        (id: int, name: str, title: str,
+         desc: str, unit: str, size: bytes, cap: str, constraint: str)"""
+        self.__device_options = {}
+        options = self.__dev.get_options()
+        return(options)
+
+    def single_page_scan(self) -> Image:
+        """single_page_scan() -> Image"""
+        self.__dev.start()
+        im = self.__dev.snap()
+        return im
+
+    def multi_page_scan(self) -> list[Image]:
+        """multi_page_scan() -> list[Image]"""
+        images = []
+        for i, pageim in enumerate(self.__dev.multi_scan()):
+            images.append(pageim)
+        return images
 
     def __del__(self):
         try:
--- src/tests/test_scanner.py
+++ src/tests/test_scanner.py
@@ -3,8 +3,26 @@ import unittest
 
 class TestScanner(unittest.TestCase):
 
-    def test_open_device(self):
-        scanner = Scanner()
+    def setUp(self):
+        self.scanner = Scanner()
+        self.scanner.initialize_device()
+
+    def test_get_device_parameters(self):
+        print(f'Testing Device parameters: {self.scanner.get_device_parameters()}')
+
+    def test_get_options(self):
+        options = self.scanner.get_options()
+        print('Testing Device Options:')
+        for option in options[0:5]:
+            print(f'\t{option}')
+
+    def test_single_page_scan(self):
+        input("Please Place a test page in the tray and then press enter to continue...")
+        self.scanner.single_page_scan()
+
+    def test_multi_page_scan(self):
+        input("Place multiple pages in the tray and press enter to continue...")
+        self.scanner.multi_page_scan()
 
 if __name__ == '__main__':
     unittest.main()
