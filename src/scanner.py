fc6e27ba0d9ee286f40753f141f04aeedbb9e45e
Joseph Bennett <jbennett@jbennettconsulting.com>
2023-07-21 11:37:25 -0400

    Initial Code

---
+++ src/scanner.py
@@ -0,0 +1,7 @@
+import sane
+import numpy
+from PIL import Image
+
+class Scanner(object):
+    def __init__(self):
+        pass
---
+++ src/tests/test_scanner.py
@@ -0,0 +1,10 @@
+from scanner import Scanner
+import unittest
+
+class TestScanner(unittest.TestCase):
+
+    def test_get_device(self):
+        scanner = Scanner()
+
+if __name__ == '__main__':
+    unittest.main()
