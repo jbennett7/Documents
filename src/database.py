ba51e51b081adc96b95e393da28765b5e66b16e0
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2023-07-31 22:21:40 -0400

    adds Documents class in database.py; updates main to use Documents

---
+++ src/database.py
@@ -0,0 +1,20 @@
+import re
+from tinydb import TinyDB, Query
+
+class Documents(object):
+
+    def __init__(self, path):
+        self._ = TinyDB(path)
+
+    def search_labels(self,regex):
+        query = re.compile(regex, re.IGNORECASE)
+        return [ item for item in self._ 
+                 if [label for label in item['labels']
+                     if query.search(label)]]
+
+    def update_labels(self, created, labels):
+        Item = Query()
+        self._.update({'labels': labels}, Item.created == created)
+
+    def __del__(self):
+        self._.close()
